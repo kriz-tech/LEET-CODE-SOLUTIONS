f55ff7e6fc0f7f8e2ce5d3d29df205f275a4d863
KRIZ <krizofficial.index@gmail.com>
2025-06-28 12:49:45 +0530

    Time: 125 ms (12.74%) | Memory: 21.1 MB (41.18%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.c
+++ 503-next-greater-element-ii/next-greater-element-ii.c
@@ -2,7 +2,7 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* nextGreaterElements(int* nums, int n, int* rs) {
-    int* res = (int*)malloc(n * sizeof(int));
+    int* res = malloc(n * sizeof(int));
     for (int i = 0; i < n; i++) {
         res[i] = -1; 
         for (int j = 1; j < n; j++) {
