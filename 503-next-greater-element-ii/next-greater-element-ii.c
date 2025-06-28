5074a27074f68fd9cca1ca4e0e87808ca07afff5
KRIZ <krizofficial.index@gmail.com>
2025-06-28 12:52:31 +0530

    Time: 118 ms (18.63%) | Memory: 21 MB (84.31%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.c
+++ 503-next-greater-element-ii/next-greater-element-ii.c
@@ -6,9 +6,8 @@ int* nextGreaterElements(int* nums, int n, int* rs) {
     for (int i = 0; i < n; i++) {
         res[i] = -1; 
         for (int j = 1; j < n; j++) {
-            int idx = (i + j) % n;
-            if (nums[idx] > nums[i]) {
-                res[i] = nums[idx];
+            if (nums[(i + j) % n] > nums[i]) {
+                res[i] = nums[(i + j) % n];
                 break;
             }
         }
