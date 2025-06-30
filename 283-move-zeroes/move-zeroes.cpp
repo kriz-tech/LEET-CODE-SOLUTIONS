ad565f248402f4dbf17b80576c7383ac00019f75
KRIZ <krizofficial.index@gmail.com>
2025-06-30 15:06:03 +0530

    Time: 2 ms (19.30%) | Memory: 23.9 MB (51.02%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int k = 0;
+        int n = nums.size();
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] != 0)
+            {
+                nums[k++] = nums[i];
+            } 
+        }
+        while(k < n) nums[k++] = 0;
+    }
+};
