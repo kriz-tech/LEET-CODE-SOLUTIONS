85bece2286d789f7188c5015314022b7c17f61f8
KRIZ <krizofficial.index@gmail.com>
2025-06-30 09:37:08 +0530

    Time: 22 ms (6.26%) | Memory: 15.2 MB (99.59%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.c
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.c
@@ -1,14 +1,9 @@
-
 int maxProfit(int* prices, int n) {
-    int min_price = prices[0];
-    int max_profit = 0;
-
-    for (int i = 1; i < n; i++) {
-        if (prices[i] < min_price)
-            min_price = prices[i];
-        else if (prices[i] - min_price > max_profit)
-            max_profit = prices[i] - min_price;
+    int minBuyprice = prices[0], max_profit = 0;
+    for(int i = 1; i < n; i++)
+    {
+        minBuyprice = fmin(prices[i], minBuyprice);
+        max_profit = fmax(max_profit, prices[i] - minBuyprice);
     }
-
     return max_profit;
-}
+} 
