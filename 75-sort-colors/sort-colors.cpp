e26adde7750c5a812d8427a06dbd65b3b76b8501
KRIZ <krizofficial.index@gmail.com>
2025-06-30 16:06:34 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (97.40%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,8 +1,21 @@
 class Solution {
 public:
+    void swap(int *p1, int *p2)
+    {
+        int t = *p1;
+        *p1 = *p2;
+        *p2 = t;
+    }
     void sortColors(vector<int>& nums) {
         int n = nums.size();
-        sort(nums.begin(), nums.end());
+        int zeroidx = 0, twoidx = n - 1;
+        int itr = 0;
 
+        while(itr <= twoidx)
+        {
+            if(nums[itr] == 0) swap(&nums[itr++], &nums[zeroidx++]);
+            else if(nums[itr] == 1) ++itr;
+            else swap(&nums[itr], &nums[twoidx--]);
+        }
     }
 };
