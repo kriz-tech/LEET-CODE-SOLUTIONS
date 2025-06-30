15acd4e069ef7821812bdf28b6c98d4f39dcca30
KRIZ <krizofficial.index@gmail.com>
2025-06-30 15:20:35 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (21.35%) - LeetSync

--- 283-move-zeroes/move-zeroes.c
+++ 283-move-zeroes/move-zeroes.c
@@ -1,4 +1,4 @@
-void moveZeroes(int* nums, int n) {
+/*void moveZeroes(int* nums, int n) {
         int k = 0;
 
         for(int i = 0; i < n; i++)
@@ -9,6 +9,16 @@ void moveZeroes(int* nums, int n) {
             } 
         }
         while(k < n) nums[k++] = 0;
-
     
+}*/
+void swap(int *p1, int* p2)
+{
+    int t = *p1;
+    *p1 = *p2;
+    *p2 = t;
+}
+
+void moveZeroes(int* nums, int n){
+    int nzi = 0;
+    for(int i = 0; i < n; i++) if(nums[i] != 0) swap(&nums[i], &nums[nzi++]);
 }
