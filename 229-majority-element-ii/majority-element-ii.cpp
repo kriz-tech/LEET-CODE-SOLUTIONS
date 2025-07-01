4684c274f75b80a09f2435d5c92f8ca10c3a9c71
KRIZ <krizofficial.index@gmail.com>
2025-07-01 15:34:07 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (76.94%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,19 +1,43 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int voter1 = 0, voter2 = 0, vc1 = 0, vc2 = 0;
+        bool flag1 = false, flag2 = true;
+
+        for(auto ele : nums)
+        {
+            if(flag1 && voter1 == ele) vc1++;
+            else if(flag2 && voter2 == ele) vc2++;
+            else if(vc1 == 0)
+            {
+                voter1 = ele;
+                flag1 = true;
+                vc1++;
+            }
+            else if(vc2 == 0)
+            {
+                voter2 = ele;
+                flag2 = true;
+                vc2++;
+            }
+            else
+            {
+                vc1--; vc2--;
+            }
+        }
+
+        int count1 = 0, count2 = 0;
+        for(auto ele : nums)
+        {
+            if(ele == voter1) ++count1;
+            else if(ele == voter2) ++count2;
+        }
+
         vector<int>res;
-        unordered_map<int, int>m;
-        for(auto i : nums) m[i] += 1;
-        for(auto i : m)
-           if(i.second > nums.size()/3) res.push_back(i.first);
+
+        if(count1>n/3)res.push_back(voter1);
+        if(count2>n/3)res.push_back(voter2);
         return res;
     }
 };
-#define LC_HACK
-#ifdef LC_HACK
-const auto __ = []() {
- struct ___ { static void _() { std::ofstream("display_runtime.txt") << 0 << '\n'; } };
- std::atexit(&___::_);
- return 0;
-}();
-#endif
