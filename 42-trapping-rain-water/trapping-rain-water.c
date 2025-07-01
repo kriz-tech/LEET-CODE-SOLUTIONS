58f98b5bafb1a93230b0a11bf91feb17bbd78e13
KRIZ <krizofficial.index@gmail.com>
2025-07-01 10:36:13 +0530

    Time: 1 ms (18.03%) | Memory: 9.8 MB (19.45%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.c
@@ -0,0 +1,10 @@
+int trap(int* h, int n) {
+    int res = 0, lmax[n], rmax[n];
+    lmax[0] = h[0];
+    for(int i = 1; i < n; i++) lmax[i] = fmax(lmax[i - 1], h[i]);
+    rmax[n - 1] = h[n - 1];
+    for(int i = n - 2; i >= 0; i--) rmax[i] = fmax(rmax[i + 1], h[i]);
+    for(int i = 0; i < n; i++) res += fmin(lmax[i],rmax[i]) - h[i];
+
+    return res;
+}
