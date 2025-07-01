02d9ea4820608db768d30bcb3e57c4721df9806a
KRIZ <krizofficial.index@gmail.com>
2025-07-01 12:41:11 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (6.29%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -3,7 +3,9 @@ public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
-        return nums[n/2];
+        for(int i = 0; i < n/2 + n % 2; i++)
+          if(nums[i] == nums[i + n/2]) return nums[i];
+        return 0;
 
     }
 };
