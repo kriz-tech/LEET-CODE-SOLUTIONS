121ed7b216320ca4ac0833d1cf728e88a16d13ee
KRIZ <krizofficial.index@gmail.com>
2025-07-04 15:23:24 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (22.29%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
-int robb(vector<int>&nums, int i, vector<int>& dp)
-{
-    if(i >= nums.size()) return 0;
-    if(dp[i] != -1) return dp[i];
-    return dp[i] = max(robb(nums, i + 1, dp), robb(nums, i + 2, dp) + nums[i]);
-}
     int rob(vector<int>& nums) {
-        vector<int>dp(nums.size(), -1);
-        return robb(nums, 0, dp);
+        int n = nums.size();
+        vector<int>dp(n+1);
+        dp[0] = 0; dp[1] = nums[0];
+
+        for(int i = 1; i < n; i++)
+            dp[i + 1] = max(dp[i], dp[i - 1] + nums[i]);
+        return dp[n];
     }
 };
