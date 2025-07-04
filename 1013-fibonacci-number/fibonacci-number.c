545263f6ea66a345eaca0cef097388692f9fb860
KRIZ <krizofficial.index@gmail.com>
2025-07-04 11:25:03 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (99.82%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.c
+++ 1013-fibonacci-number/fibonacci-number.c
@@ -1,6 +1,8 @@
+//Topdown Memorization...
+int f[31] = {0, 1};
 int fib(int n){
-    if(n == 0) return 0;
-    if(n == 1) return 1;
-
-    return fib(n - 1) + fib(n - 2);
+    if(n <= 1) return n;
+    if(f[n] != 0) return f[n];
+    f[n] = fib(n - 1) + fib(n - 2);
+    return f[n];
 }
