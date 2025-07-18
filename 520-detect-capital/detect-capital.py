8aad3eb2e28b183155dec04d205ad1eb4032cb16
Krishnavel P <krizofficial.index@gmail.com>
2025-07-18 19:30:28 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (7.81%) - LeetSync

---
+++ 520-detect-capital/detect-capital.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def detectCapitalUse(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        return word.islower() or word.isupper() or (word[0].isupper() and word[1:].islower())
+        
