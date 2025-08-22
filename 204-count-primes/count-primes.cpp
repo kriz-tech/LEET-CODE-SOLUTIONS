c5e00a9f4199d2de135774431a5822ac3bad2610
Krishnavel P <krizofficial.index@gmail.com>
2025-08-22 21:24:55 +0530

    Time: 261 ms (12.70%) | Memory: 159.6 MB (9.97%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        if(n < 3) return 0;
+        vector<int>primes;
+        vector<int>lp(n+1, 0);
 
-        vector<bool>isprime(n, true);
-        isprime[0] = isprime[1] = false;
-
-        for(int i = 2; i * i < n; i++){
-            if (isprime[i]){
-                for(int j = i * i; j < n; j+=i){
-                    isprime[j] = false;
-                }
+        for(int i = 2; i < n; i++){
+            if(lp[i] == 0){
+                lp[i] = i;
+                primes.push_back(i);
+            }
+            for(int p : primes){
+                if(p > lp[i] || p * i >= n) break;
+                lp[p * i] = p;
             }
         }
-        return count(isprime.begin(), isprime.end(), true);
+        return primes.size();
     }
 };
