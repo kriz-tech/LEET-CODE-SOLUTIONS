17a64ac0da42e8d078fcabd20df4c756e3795521
Krishnavel P <krizofficial.index@gmail.com>
2026-07-29 22:31:29 +0530

    Time: 3 ms (88.88%) | Memory: 23.9 MB (30.95%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -6,7 +6,7 @@ public:
         vector<vector<int>> res;
 
         res.push_back(intervals[0]);
-        for(int i = 0; i < intervals.size(); i++){
+        for(int i = 1; i < intervals.size(); i++){
             if(res.back()[1] >= intervals[i][0]){
                 res.back()[1] = max(res.back()[1], intervals[i][1]);
             }
